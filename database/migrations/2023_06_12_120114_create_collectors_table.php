4dc11f0ab4387c9b25d96c6d4e98cbb3d551df46
sarah <24282945+sarahdelossantos@users.noreply.github.com>
2023-06-19 20:42:20 +0800

    Update 2023_06_12_120114_create_collectors_table.php

--- database/migrations/2023_06_12_120114_create_collectors_table.php
+++ database/migrations/2023_06_12_120114_create_collectors_table.php
@@ -17,13 +17,9 @@ class CreateCollectorsTable extends Migration
             $table->id();
             $table->integer('user_id');
             $table->string('code')->nullable();
-            // $table->string('fullname');
-            // $table->string('mobile');
-            // $table->string('address');
             $table->double('cashbond',8,2)->nullable();
             $table->string('ctc_no')->nullable();
             $table->string('status')->default(1)->comment('0 - pending, 1 - active, 2 - inactive');
-            // $table->string('row_status')->default(0)->comment('0 - pending, 1 - approved, 2 - rejected, 3 - archived');
             $table->timestamps();
         });
     }
